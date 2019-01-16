dd82e8003dec4c7df47a0bf420165caeff0f1ee4
baaesh <sanghwan@europa.snu.ac.kr>
2019-01-16 14:49:06 +0900

    vocab2txt

--- san/data/vocab2txt.py
+++ san/data/vocab2txt.py
@@ -9,7 +9,7 @@ TEXT.vocab = pickle.load(filehandler)
 
 with open('vocab.txt', 'w', encoding='utf-8') as f:
     for i in range(len(TEXT.vocab)):
-        str = ''.join(c for c in TEXT.vocab.itos[i] if c in emoji.UNICODE_EMOJI)
+        str = ''.join(c for c in TEXT.vocab.itos[i])
         if str != '':
             f.write(str + '\n')
 
