252d2c2f2264687315353d81c112d1188759ae42
baaesh <sanghwan@europa.snu.ac.kr>
2019-01-25 16:25:46 +0900

    checking the submission file

---
+++ san/check_submission.py
@@ -0,0 +1,26 @@
+import os
+import io
+import random
+
+dataFilePath = 'final_submissions/best.txt'
+
+with io.open(dataFilePath, encoding="utf8") as finput:
+    finput.readline()
+    for line in finput:
+        line_split = line.strip().split('\t')
+        try:
+            label = line_split[4]
+        except:
+            print(line_split)
+
+        if label == 'others':
+            pass
+        elif label == 'happy':
+            pass
+        elif label == 'angry':
+            pass
+        elif label == 'sad':
+            pass
+        else:
+            print(line_split[0])
+            print(label)
